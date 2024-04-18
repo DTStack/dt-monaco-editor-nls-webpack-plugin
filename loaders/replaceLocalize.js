e90cb130051fa6d3b0662646bcaadf7f1769ad08
琉易 <liuxy0551@qq.com>
2024-04-18 10:25:20 +0800

    fix: #3 compatible Windows and chrome v65 (#4)

--- loaders/replaceLocalize.js
+++ loaders/replaceLocalize.js
@@ -6,7 +6,7 @@ module.exports = function (content, _map, _meta) {
     if (!vsPath) return content;
 
     // add vscode-loc path to function localize
-    const path = vsPath.replace(/\\/g, '/').replace('.js', '');
+    const path = vsPath.replace(/\\/g, "/").replace(".js", "");
     return content.replace(/(\bfunction\s+localize\()|(\blocalize\()/g, function (text) {
         if (/function\s+localize/.test(text)) return text;
         return `localize('${path}', `;
--- loaders/replaceNls.js
+++ loaders/replaceNls.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const nls = require.resolve('../nls.js');
+const fs = require("fs");
+const nls = require.resolve("../nls.js");
 
 module.exports = function (content, _map, _meta) {
-    const regExp = /monaco-editor\/esm\/vs\/nls\.js/;
+    const pathRegExp = /monaco-editor[\\\/]esm[\\\/]vs[\\\/]nls\.js$/;
 
-    if (!regExp.test(this.resourcePath)) return content;
-    const _content = fs.readFileSync(nls, { encoding: 'utf8' });
+    if (!pathRegExp.test(this.resourcePath)) return content;
+    const _content = fs.readFileSync(nls, { encoding: "utf8" });
     return _content;
 };
